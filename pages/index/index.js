e14c1d3d3bcae90b97eebae9c5435792b401c4bf
廖星 <xing.liao@wutnews.net>
2018-01-28 14:35:03 +0800

    完成自定义导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,10 @@ Page({
     course: data.course,
     week: 21,
     start: data.start,
-
   },
-  
+  bindWeekChange(e) {
+    this.setData({
+      week: parseInt(e.detail.value) + 1
+    });
+  }
 })
