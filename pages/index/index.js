a3326e2ffc2a8d76faa5b35fd90d4f0b2b9a8e62
廖星 <xing.liao@wutnews.net>
2018-01-28 17:20:35 +0800

    完成课程详情弹窗

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,40 @@ Page({
     course: data.course,
     week: 21,
     start: data.start,
+    current: {}
   },
   bindWeekChange(e) {
     this.setData({
       week: parseInt(e.detail.value) + 1
+    });
+  },
+  showCourseDetail(e) {
+    const course = e.currentTarget.dataset.course;
+    const self = this;
+
+    if (course.length === 1) {
+      self.setData({
+        current: course[0]
+      });
+      return;
+    }
+
+    let name_arr = course.map(item => {
+      return item.name
+    });
+
+    wx.showActionSheet({
+      itemList: name_arr,
+      success(res) {
+        self.setData({
+          current: course[res.tapIndex]
+        });
+      }
+    });
+  },
+  hideCourseDetail() {
+    this.setData({
+      current: {}
     });
   }
 })
